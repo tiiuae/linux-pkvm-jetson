aosp-mirror/kernel_common

ANDROID: KVM: arm64: Allow trap handling from pKVM modules
Introduce a new default trap handler for the host that can be set
from modules.

Bug: 244543039
Bug: 245034629
Change-Id: Iaabfa44f5f2c41af51f36ed4eec8762e7c951c01
Signed-off-by: Quentin Perret <qperret@google.com>

arch/arm64/include/asm/kvm_pkvm_module.h
@@ -27,6 +27,7 @@ struct pkvm_module_ops {
 	int (*register_host_perm_fault_handler)(int (*cb)(struct kvm_cpu_context *ctxt, u64 esr, u64 addr));
 	int (*protect_host_page)(u64 pfn, enum kvm_pgtable_prot prot);
 	int (*register_host_smc_handler)(bool (*cb)(struct kvm_cpu_context *));
+	int (*register_default_trap_handler)(bool (*cb)(struct kvm_cpu_context *));
 	int (*register_illegal_abt_notifier)(void (*cb)(struct kvm_cpu_context *));
 	int (*register_psci_notifier)(void (*cb)(enum pkvm_psci_notification, struct kvm_cpu_context *));
 };

arch/arm64/kvm/hyp/include/nvhe/modules.h
@@ -4,6 +4,7 @@
 #define HCALL_UNHANDLED -1
 
 int __pkvm_register_host_smc_handler(bool (*cb)(struct kvm_cpu_context *));
+int __pkvm_register_default_trap_handler(bool (*cb)(struct kvm_cpu_context *));
 int __pkvm_register_illegal_abt_notifier(void (*cb)(struct kvm_cpu_context *));
 
 enum pkvm_psci_notification;

tiiuae/linux-pkvm-jetson

Bug: 458241298
Signed-off-by: Fuad Tabba <tabba@google.com>

arch/arm64/include/asm/kvm_pkvm_module.h
@@ -28,6 +28,7 @@ struct pkvm_module_ops {
 	void (*flush_dcache_to_poc)(void *addr, size_t size);
 	int (*register_host_perm_fault_handler)(int (*cb)(struct user_pt_regs *regs, u64 esr, u64 addr));
 	int (*register_host_smc_handler)(bool (*cb)(struct user_pt_regs *));
+	int (*register_default_trap_handler)(bool (*cb)(struct user_pt_regs *));
 	int (*register_illegal_abt_notifier)(void (*cb)(struct user_pt_regs *));
 	int (*register_psci_notifier)(void (*cb)(enum pkvm_psci_notification, struct user_pt_regs *));
 };

arch/arm64/kvm/hyp/include/nvhe/modules.h
@@ -4,6 +4,7 @@
 #define HCALL_UNHANDLED -1
 
 int __pkvm_register_host_smc_handler(bool (*cb)(struct user_pt_regs *));
+int __pkvm_register_default_trap_handler(bool (*cb)(struct user_pt_regs *));
 int __pkvm_register_illegal_abt_notifier(void (*cb)(struct user_pt_regs *));
 
 enum pkvm_psci_notification;